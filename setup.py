193f520ccf403dbafe000dade2ce771bb47c3ddf
Niboon Boonprakob <30637687+niboon39@users.noreply.github.com>
2024-09-04 01:52:30 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='heic_to_jpeg_converter',
+    name='HeicToJpg',
     version='1.0',
     packages=find_packages(),
     install_requires=[
@@ -12,7 +12,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'heic_to_jpeg_converter=your_module:main_function',
+            'HeicToJpg=your_module:main_function',
         ],
     },
     author='Niboon',
